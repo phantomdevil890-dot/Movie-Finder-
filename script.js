95553f889461e2bd0e35248b90353d148cdd886f
phantomdevil890-dot <phantomdevil890@gmail.com>
2026-04-10 14:01:22 +0600

    script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+async function searchMovie() {
+  const movieName = document.getElementById("searchInput").value;
+  const resultDiv = document.getElementById("movieResult");
+
+  if (!movieName) {
+    resultDiv.innerHTML = "❌ Please enter a name";
+    return;
+  }
+
+  resultDiv.innerHTML = "⏳ Loading...";
+
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${encodeURIComponent(movieName)}`);
+    const data = await response.json();
+
+    if (data.length === 0) {
+      resultDiv.innerHTML = "❌ No result found";
+      return;
+    }
+
+    const show = data[0].show;
+
+    resultDiv.innerHTML = `
+      <h2>${show.name}</h2>
+      <img src="${show.image ? show.image.medium : ''}" />
+      <p><b>Language:</b> ${show.language}</p>
+      <p><b>Genres:</b> ${show.genres.join(", ")}</p>
+      <p><b>Rating:</b> ${show.rating.average}</p>
+      <p><b>Summary:</b> ${show.summary}</p>
+    `;
+  } catch (error) {
+    resultDiv.innerHTML = "⚠️ Error fetching data";
+  }
+                          }
